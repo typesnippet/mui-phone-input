4f4b78767ff9719472b678de03b57bdd11d8a594
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2023-12-26 14:28:58 +0400

    GH-5: Implement Jest executor by given environment

--- scripts/execute-jest.ts
+++
@@ -1,7 +0,0 @@
-import process from "node:child_process";
-
-import jestconfig from "../jestconfig.json";
-
-const spawn = (cmd: string, args: string[]) => process.spawn(cmd, args, {shell: "/bin/bash", stdio: "inherit"});
-
-spawn("./node_modules/.bin/jest", ["--config", `'${JSON.stringify(jestconfig)}'`]);
