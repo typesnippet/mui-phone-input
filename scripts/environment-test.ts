ca6cba0a5212638c03b34381639760bc3fa6de80
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2024-01-02 17:37:44 +0400

    GH-5: Get rid of temporary build mechanism

--- scripts/environment-test.ts
+++
@@ -1,7 +0,0 @@
-import child_process from "node:child_process";
-
-import jestConfig from "../jestconfig.json";
-
-const spawn = (cmd: string, args: string[]) => child_process.spawn(cmd, args, {shell: "/bin/bash", stdio: "inherit"});
-const config = {...jestConfig, moduleNameMapper: {"@mui/material": process.argv[2]}};
-spawn("jest", ["--config", `'${JSON.stringify(config)}'`]).on("exit", process.exit);
