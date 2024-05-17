cb73c2d01c589a00ef3711c1488efc1fcaf1b379
Artyom Vancyan <artyom.vancyan2000@gmail.com>
2024-05-17 19:03:02 +0400

    Add the `use client` directive for SSR

--- src/base/index.tsx
+++ src/base/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {ChangeEvent, forwardRef, KeyboardEvent, useCallback, useEffect, useRef, useState} from "react";
 import {Input as BaseInput, InputProps} from "@mui/base/Input";
 
